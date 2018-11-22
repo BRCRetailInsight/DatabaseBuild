f6df6a86f841d6fe48303d3ea6a1a885dd43d40e
James <james.hardiman@brc.org.uk>
2018-11-22 16:02:16 +0000

    Most of GDP, half of Labour market stuff

---
+++ APIgetGDP.R
@@ -0,0 +1,51 @@
+library(readxl)
+library(bsts)
+library(xts)
+library(dplyr)
+library(pdfetch)
+
+#### Get Monthly GDP Data ####
+
+# Time Series Definitions
+
+# "ECYX" = Monthly GVA Month on Month (CVM SA %)
+# "ED2R" = Monthly GVA Year on Year (CVM SA %)
+
+# ONS Monthly GDP - (mgdp)
+GVAmonthly <- pdfetch_ONS(c("ECYX", "ED2R"), "mgdp")
+
+GVAmonthly_yoy <- GVAmonthly$ED2R
+
+GVAmonthly_mom <- GVAmonthly$ECYX
+
+
+#### ONS Quarterly GDP Data - qna ####
+
+# Time Series Definitions
+
+# "ABMM" = Quarterly GVA (CVM SA £m)
+
+#GVA at Basic Prices - qna
+gvaquarterly <- pdfetch_ONS(c("ABMM"), "qna")
+colnames(gvaquarterly) <- "Whole Economy"
+
+#### ONS Quartlery GVA, from: "GDP output approach - Low Level Aggregates" ####
+
+#GVA Whole Economy (CP £m)
+url <- "https://www.ons.gov.uk/file?uri=/economy/grossdomesticproductgdp/datasets/ukgdpolowlevelaggregates/current/gdplla.xls"
+loc.download <- "gva_all.xls"
+download.file(url,loc.download,mode = "wb")
+gva_all <- "gva_all.xls"
+gva_all <- read_excel(gva_all, sheet = 3, range = "D46:D132")
+
+dates <- seq(as.Date("1997-03-20"), length = nrow(gva_all), by = "quarters")
+dates <- LastDayInMonth(dates)
+gva_all <- xts(x = gva_all, order.by=dates)
+colnames(gva_all) <- "Whole Economy"
+
+#GVA Retail (CP £m)
+gva_retail <- "gva_all.xls"
+gva_retail <- read_excel(gva_retail, sheet = 3, range = "CY46:CY132")
+gva_retail <- xts(x = gva_retail, order.by=dates)
+colnames(gva_retail) <- "Retail"
+
--- APIgetOUTPUT.R
+++ APIgetOUTPUT.R
@@ -5,7 +5,7 @@ library(dplyr)
 
 #### Download Output Excel File and Create XTS ####
 
-#Output Whole Economy
+#Output per hour Whole Economy
 url <- "https://www.ons.gov.uk/file?uri=/employmentandlabourmarket/peopleinwork/labourproductivity/datasets/annualbreakdownofcontributionswholeeconomyandsectors/current/prodconts.xls"
 loc.download <- "output_all.xls"
 download.file(url,loc.download,mode = "wb")
@@ -17,7 +17,7 @@ dates <- LastDayInMonth(dates)
 output_all <- xts(x = output_all, order.by=dates)
 colnames(output_all) <- "Whole Economy"
 
-#Output Retail
+#Output per hour Retail
 url <- "https://www.ons.gov.uk/file?uri=/economy/economicoutputandproductivity/productivitymeasures/datasets/labourproductivitybyindustrydivision/apriltojune2018/division.xls"
 loc.download <- "output_retail.xls"
 download.file(url,loc.download,mode = "wb")
@@ -29,21 +29,3 @@ dates <- LastDayInMonth(dates)
 output_retail <- xts(x = output_retail, order.by=dates)
 colnames(output_retail) <- "Retail"
 
-
-#GVA Whole Economy
-url <- "https://www.ons.gov.uk/file?uri=/economy/grossdomesticproductgdp/datasets/ukgdpolowlevelaggregates/current/gdplla.xls"
-loc.download <- "gva_all.xls"
-download.file(url,loc.download,mode = "wb")
-gva_all <- "gva_all.xls"
-gva_all <- read_excel(gva_all, sheet = 3, range = "D46:D132")
-
-dates <- seq(as.Date("1997-03-20"), length = nrow(gva_all), by = "quarters")
-dates <- LastDayInMonth(dates)
-gva_all <- xts(x = gva_all, order.by=dates)
-colnames(gva_all) <- "Whole Economy"
-
-#GVA Retail
-gva_retail <- "gva_all.xls"
-gva_retail <- read_excel(gva_retail, sheet = 3, range = "CY46:CY132")
-gva_retail <- xts(x = gva_retail, order.by=dates)
-colnames(gva_retail) <- "Retail"
